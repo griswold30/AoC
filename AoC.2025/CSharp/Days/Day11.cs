9e489ac40e18e0799bccf7972a53e996ebeecaf4
13556154+griswold30@users.noreply.github.com <13556154+griswold30@users.noreply.github.com>
2025-12-12 00:48:19 -0600

    Adds 2025 csharp day 11 Part 2 and fixes Part 1

--- AoC.2025/CSharp/Days/Day11.cs
+++ AoC.2025/CSharp/Days/Day11.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using AoC.Shared;
+﻿using AoC.Shared;
+using System.ComponentModel;
 
 namespace AoC._2025.Days
 {
@@ -18,45 +17,132 @@ namespace AoC._2025.Days
                 var thisDeviceString = devicesString[0][..^1];
                 var outputDeviceStrings = devicesString[1..];
 
+                var outputDevices = outputDeviceStrings.Select(x => devices.ContainsKey(x) ? devices[x] : new Device(x, new List<Device>()));
+
+                // inefficient, but graph size likely doesn't matter here.
+                foreach (var oDevice in outputDevices)
+                {
+                    devices.TryAdd(oDevice.Name, oDevice);
+                }
+
                 if (devices.ContainsKey(thisDeviceString))
                 {
-                    devices[thisDeviceString].OutputDevices.AddRange(outputDeviceStrings.Select(x => devices.ContainsKey(x) ? devices[x] : new Device(x, new List<Device>())).ToList());
+                    devices[thisDeviceString].OutputDevices.AddRange(outputDevices);
                 }
                 else
                 {
-                    var thisDevice = new Device(thisDeviceString, outputDeviceStrings.Select(x => devices.ContainsKey(x) ? devices[x] : new Device(x, new List<Device>())).ToList());
+                    var thisDevice = new Device(thisDeviceString, outputDevices.ToList());
                     devices[thisDeviceString] = thisDevice;
                 }
             }
             var countOfPathsThroughYou = 0;
             var visited = new HashSet<Device>();
+            Dictionary<string, int> memo = new();
 
-            CountPaths(devices["svr"], ref countOfPathsThroughYou, devices, visited);
+            foreach (var device in devices["you"].OutputDevices)
+            {
+                countOfPathsThroughYou += CountPaths(device, devices, memo);
+            }
 
             return countOfPathsThroughYou.ToString();
         }
 
-        public bool CountPaths(Device device,ref int paths, Dictionary<string, Device> devices, HashSet<Device> visited)
+
+
+        public string Part2(string input)
         {
+            var lines = input.Split('\n');
 
-            if (device.Name == "out")
-                return true;
+            var devices = new Dictionary<string, Device>();
 
-            foreach (var outputDevice in device.OutputDevices)
+            foreach (var line in lines)
             {
-                var thisDevice = devices.ContainsKey(outputDevice.Name) ? devices[outputDevice.Name] : outputDevice;
-                if (CountPaths(thisDevice, ref paths, devices, visited))
+                var devicesString = line.Split(' ');
+                var thisDeviceString = devicesString[0][..^1];
+                var outputDeviceStrings = devicesString[1..];
+
+                var outputDevices = outputDeviceStrings.Select(x => devices.ContainsKey(x) ? devices[x] : new Device(x, new List<Device>()));
+
+                // inefficient, but graph size likely doesn't matter here.
+                foreach (var oDevice in outputDevices)
                 {
-                    if (!visited.Contains(thisDevice)) visited.Add(thisDevice);
-                    return true;
+                    devices.TryAdd(oDevice.Name, oDevice);
+                }
+
+                if (devices.ContainsKey(thisDeviceString))
+                {
+                    devices[thisDeviceString].OutputDevices.AddRange(outputDevices);
+                }
+                else
+                {
+                    var thisDevice = new Device(thisDeviceString, outputDevices.ToList());
+                    devices[thisDeviceString] = thisDevice;
+                }
+            }
+            var countOfPathsThroughYou = 0L;
+            var visited = new HashSet<Device>();
+            Dictionary<(string, bool, bool), long> memo = new();
+
+            foreach (var device in devices["svr"].OutputDevices)
+            {
+                countOfPathsThroughYou += CountPathsWithFftDac(device, devices, memo, false, false);
+            }
+
+            return countOfPathsThroughYou.ToString();
+        }
+
+        public long CountPathsWithFftDac(Device device, Dictionary<string, Device> devices, Dictionary<(string, bool, bool), long> memo, bool fft, bool dac)
+        {
+            if (device.Name == "out" && fft && dac)
+                return 1;
+
+            if (device.Name == "fft") fft = true;
+            if (device.Name == "dac") dac = true;
+
+            var numReachedOut = 0l;
+
+            if (memo.ContainsKey((device.Name, fft, dac)))
+            {
+                return memo[(device.Name, fft, dac)];
+            }
+            else
+            {
+                foreach (var outputDevice in device.OutputDevices)
+                {
+                    var thisDevice = devices.ContainsKey(outputDevice.Name) ? devices[outputDevice.Name] : outputDevice;
+                    numReachedOut += CountPathsWithFftDac(thisDevice, devices, memo, fft, dac);
                 }
             }
 
-            return false;
+            memo[(device.Name, fft, dac)] = numReachedOut;
+            return numReachedOut;
         }
 
+        public int CountPaths(Device device, Dictionary<string, Device> devices, Dictionary<string, int> memo)
+        {
+            if (device.Name == "out")
+                return 1;
 
-        public record Device
+            var numReachedOut = 0;
+            
+            if (memo.ContainsKey(device.Name))
+            {
+                return memo[device.Name];
+            }
+            else
+            {
+                foreach (var outputDevice in device.OutputDevices)
+                {
+                    var thisDevice = devices.ContainsKey(outputDevice.Name) ? devices[outputDevice.Name] : outputDevice;
+                    numReachedOut += CountPaths(thisDevice, devices, memo);
+                }
+            }
+
+            memo[device.Name] = numReachedOut;
+            return numReachedOut;
+        }
+
+        public class Device
         {
             public List<Device> OutputDevices = new List<Device>();
             public string Name { get; set; }
@@ -66,11 +152,6 @@ namespace AoC._2025.Days
                 Name = name;
                 OutputDevices = output;
             }
-        }
-
-        public string Part2(string input)
-        {
-            throw new NotImplementedException();
         }
     }
 }
